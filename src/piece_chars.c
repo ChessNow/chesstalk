d0af1c364ec65c40b1418dfe5afa3d95197c07de
Chess Now <chessnow@simulation.phrasep.com>
2010-10-02 00:13:03 -0400

    update for piece_name to use the piece_chars split out, perhaps overkill.

---
+++ src/piece_chars.c
@@ -0,0 +1,5 @@
+
+// must match the sequence set out in piece_names.c
+
+char *piece_chars = "NBKRQ";
+
---
+++ src/piece_chars.h
@@ -0,0 +1,6 @@
+#ifndef PIECE_CHARS_H
+#define PIECE_CHARS_H
+
+extern char *piece_chars;
+
+#endif
